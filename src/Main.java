5c6aefdaee506bb8389892692f78e1a649de8a70
Augustus100 <augustusrossi14@gmail.com>
2026-02-26 09:44:43 -0300

    Adição de classes, Curso, Mentoria e implementação básica no Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso JAVA");
+        curso1.setDescricao("Descricao curso java");
+        curso1.setCargaHoraria(8);
+
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descricao curso Javascript");
+        curso2.setCargaHoraria(20);
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria java");
+        mentoria1.setDescricao("mentoria description");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+
     }
 }
